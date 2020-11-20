62b3447a0d41dfd3bc0fa81f25100b2e3235632e
Tehreem <anjumt560@gmail.com>
2020-11-20 08:01:12 +0500

    'studentapp'

--- screens/BookmarkScreen.js
+++ screens/BookmarkScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Card } from 'react-native';
+import { View, Text, Button, StyleSheet, Card,TouchableOpacity } from 'react-native';
 
-const BookmarkScreen = () => {
+const BookmarkScreen = (navigation) => {
     return (
       <View style={styles.container}>
-        <Text>My Route</Text>
-        
-        <Button
-          title="Schedule Again"
-        />
+        <TouchableOpacity style={styles.card}>
+          <Text>Route no. 1 </Text>
+          <Text>Stop: Shahdra</Text>
+          <Text>Time: 7:00 AM</Text>
+          <Button
+            title='Track Route'
+            onPress={()=>navigation.navigate('ExploreScreen')}></Button>
+        </TouchableOpacity>
       </View>
     );
 };
@@ -20,5 +23,17 @@ const styles = StyleSheet.create({
     flex: 1, 
     alignItems: 'center', 
     justifyContent: 'center'
+  },
+  card: {
+    backgroundColor: '#ffff',
+    marginBottom: 10,
+    marginLeft: '2%',
+    width:'96%',
+    shadowColor: '#0000',
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 3,
+      heigth: 3,
+    }
   },
 });
